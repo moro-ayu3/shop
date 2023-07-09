4fe02d3daa9dec27ba790019bddcc1224ccc6e6b
moro_ayu <morohoshi3ayumi@gmail.com>
2023-07-09 09:48:28 +0900

    third-commit

---
+++ src/database/migrations/2023_07_09_090141_create_reserves_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReservesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reserves', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('test_id')->constrained('tests');
+            $table->date('date');
+            $table->time('time');
+            $table->integer('number');
+            $table->timestamp('created_at')->useCurrent()->nullable();
+            $table->timestamp('updated_at')->useCurrent()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reserves');
+    }
+}
